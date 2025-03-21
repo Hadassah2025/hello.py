658e0a64e5305f3fe402a258c15daf678ecfd6c1
omolara <ask4lari@gmail.com>
2025-03-21 09:50:38 +0100

    commit all changes

---
+++ strings.py
@@ -0,0 +1,10 @@
+first_name = "Ada"
+last_name = "Ifeoma"
+full_name = f" {first_name} {last_name} "
+print(full_name)
+print(full_name.upper())
+print(full_name.lower())
+print(full_name.title())
+print(full_name.capitalize())
+print(full_name.strip())
+print(first_name.replace("Ada", "Chisom"))
