fea94312acb6bfcf0d66a3546babe4e11c406666
omolara <ask4lari@gmail.com>
2025-03-21 19:20:48 +0100

    first commit

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("Hello world!!!")
+# Variables in python
+# Data types in python
+# Strings
+# Integers
+# Floats
+# Boolean
+# List
+# Tuple
+# Set
+# Dictionary
+# Complex
---
+++ intergers.py
@@ -0,0 +1,27 @@
+# Integers
+num1 = 3.69
+num2 = 2.5
+n_division = num2 / num1
+f_division = num2 // num1
+m_division = num2 % num1
+print(f'Normal division: {n_division}')
+print(f'Floor division: {f_division}')
+print(f'Modulo division: {m_division}')
+
+# Multiplication
+multi = num1 * num2
+#Exponential
+exp = num1 ** num2 # 3 raise to power 11
+
+print(multi)
+print(round(exp, 4))
+
+
+# Type conversion in python
+num1 = 28.9
+num2 = 28
+print((int(num1 + 2)))
+print((float(num2)))
+print((str(num2) + 2))
+print((bool(num2)))
+print((bool(num1)))
