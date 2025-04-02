de5ad8d28654aea27f744c2156347d5ffdc552b8
omolara <ask4lari@gmail.com>
2025-04-03 00:03:28 +0100

    commit all changes

---
+++ voting.py
@@ -0,0 +1,28 @@
+# A python program that build software program for voting
+# User input in python
+name = input("Enter your fullname: ")
+age = int(input("Enter your age: "))
+party = input("Enter your party(party A or party B): ")
+
+if age >= 18:
+            print(f"Yes! {name}, You are eligible to vote.")
+else:        
+            print(f" No! You are not eligible to vote.")
+        
+# Voting System
+name = input("Enter your fullname: ")
+age = int(input("Enter your age: "))
+party = input("Enter the party you want to vote for (party A or party B): ")
+
+
+
+
+
+
+
+
+
+
+
+
+
